0b2a2cc3d026333fcc7c8dc8fda323b11f3d687f
Congwen Ma <Congwen.Ma@centro.net>
2017-01-10 18:47:46 -0600

    initial version of importer

---
+++ spec/importer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ImporterView from '../lib/importer-view';
+
+describe('ImporterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
